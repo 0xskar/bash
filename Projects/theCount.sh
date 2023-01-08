dd90831df3c707f19c5ebaae6ada8c67409e6b0f
0xskar <0xskar@proton.me>
2023-01-08 03:07:42 -0800

    theCount.sh

--- Projects/theCount.sh
+++ Projects/theCount.sh
@@ -9,3 +9,10 @@
 
 read -p "Enter File To Count: " count_file
 
+lines=$(wc -l $count_file | awk '{print $1}')
+words=$(wc -w $count_file | awk '{print $1}')
+chars=$(wc -m $count_file | awk '{print $1}')
+
+echo "$count_file has $lines lines."
+echo "$count_file has $words words."
+echo "$count_file has $chars characters."
