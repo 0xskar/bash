91a6bdecb1f9d402d6d8b5606219ceb1a5679df1
0xskar <0xskar@proton.me>
2023-01-11 11:55:50 -0800

    k

--- Projects/randomPass.sh
+++ Projects/randomPass.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 #
-# randomPass.sh
+#randomPass.sh
 #
-# A script that generates a random password and displays it to the user.
+#A script that generates a random password and displays it to the user.
 # 
-# 0xskar 01/08/23
+#0xskar 01/08/23
+
+
+
+
+#generate random chars
+$pass = cat /dev/random | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1
+
+
 
